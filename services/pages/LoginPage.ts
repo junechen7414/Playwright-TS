286176aaf1c2b1ce1d7bbd97b7b705c934144979
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2025-10-20 20:42:01 +0800

    Add login page and login test cases

---
+++ services/pages/LoginPage.ts
@@ -0,0 +1,28 @@
+import type { Page } from '@playwright/test';
+
+export class LoginPage {
+    readonly page: Page;
+    readonly usernameInput;
+    readonly passwordInput;
+    readonly loginButton;
+    readonly errorMessage;
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameInput = page.locator('[data-test="username"]');
+        this.passwordInput = page.locator('[data-test="password"]');
+        this.loginButton = page.locator('[data-test="login-button"]');
+        this.errorMessage = page.locator('[data-test="error"]');
+    }
+    async goto() {
+        await this.page.goto('https://www.saucedemo.com/');
+    }
+    async login(username: string, password: string) {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+    async getErrorMessage() {
+        return await this.errorMessage.textContent();
+    }
+}
+export default LoginPage;
---
+++ tests/Login.spec.ts
@@ -0,0 +1,19 @@
+import LoginPage from '../services/pages/LoginPage.js';
+import { test, expect } from '@playwright/test';
+
+test.describe('Login Tests', () => {
+    let loginPage: LoginPage;
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
+    });
+    test('Successful Login with Valid Credentials', async () => {
+        await loginPage.login('standard_user', 'secret_sauce');
+        await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    });
+    test('Unsuccessful Login with Invalid Credentials', async () => {
+        await loginPage.login('invalid_user', 'invalid_password');
+        const errorMessage = await loginPage.getErrorMessage();
+        expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+    });
+});
