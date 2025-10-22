e56d5e19fc9b7d9c39df15a1a7374309952bb901
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2025-10-22 20:35:08 +0800

    comments out redundant settings in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,14 +13,19 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  // forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 1,
+  // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 2,
+  // workers: process.env.CI ? 3 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -30,6 +35,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -8,15 +8,27 @@ test.describe('Login Tests', () => {
         await loginPage.goto();
     });
     test.describe('Successful Login Scenarios', () => {
-    test('Successful Login with Valid Credentials', async () => {
-        await loginPage.login('standard_user', 'secret_sauce');
-        await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html');
-    });
+        test('Successful Login with Valid Credentials', async () => {
+            await loginPage.login('standard_user', 'secret_sauce');
+            await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html');
+        });
     });
     test.describe('Unsuccessful Login Scenarios', () => {
-    test('Unsuccessful Login with Invalid Credentials', async () => {
-        await loginPage.login('invalid_user', 'invalid_password');
-        const errorMessage = await loginPage.getErrorMessage();
-        expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+        test('Unsuccessful Login with Invalid Credentials', async () => {
+            await loginPage.login('invalid_user', 'invalid_password');
+            const errorMessage = await loginPage.getErrorMessage();
+            expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+        });
+        test('Unsuccessful Login with Locked out Credentials', async () => {
+            await loginPage.login('locked_out_user', 'secret_sauce');
+            const errorMessage = await loginPage.getErrorMessage();
+            expect(errorMessage).toBe('Epic sadface: Sorry, this user has been locked out.');
+        });
+        test('Bypass Login Attempt', async () => {
+            let targetUrl = '/inventory.html';
+            await loginPage.bypassLogin(targetUrl);
+            const errorMessage = await loginPage.getErrorMessage();
+            expect(errorMessage).toBe(`Epic sadface: You can only access '${targetUrl}' when you are logged in.`);
+        });
     });
 });
