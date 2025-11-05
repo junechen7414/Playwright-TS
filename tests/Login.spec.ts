dba177a868ae5dd3bce2b467d16db5f610af7bf4
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2025-11-06 07:44:17 +0800

    移除會在shopping流程用到的成功登入流程test case

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,11 +1,5 @@
-import { expect, test } from '../fixtures/chainFixtures.js';
+import { test } from '../fixtures/chainFixtures.js';
 
-test.describe('Successful Login Scenarios', () => {
-    test('Successful Login with Valid Credentials', async ({ loginPage, standardUserData }) => {
-        await loginPage.login(standardUserData.username, standardUserData.password);
-        await expect(loginPage.page).toHaveURL('https://www.saucedemo.com/inventory.html');
-    });
-});
 test.describe('Unsuccessful Login Scenarios', () => {
     test('Unsuccessful Login with Invalid Credentials', async ({ loginPage, invalidUserData }) => {
         await loginPage.login(invalidUserData.username, invalidUserData.password);
