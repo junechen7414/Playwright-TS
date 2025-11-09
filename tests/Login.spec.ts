7a3f8c6ee82e279aed1c645b79a6520d46d0cce6
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2025-11-09 19:47:31 +0800

    merge 3 negative test case into 1

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,17 +1,12 @@
 import { test } from '../fixtures/chainFixtures.js';
 
 test.describe('Unsuccessful Login Scenarios', () => {
-    test('Unsuccessful Login with Invalid Credentials', async ({ loginPage, invalidUserData }) => {
+    test('Unsuccessful Login with Invalid Credentials, Locked out Credentials and Bypass Login Attempt', async ({ loginPage, invalidUserData,lockedUserData }) => {
         await loginPage.login(invalidUserData.username, invalidUserData.password);
         await loginPage.verifyErrorMessage('Epic sadface: Username and password do not match any user in this service');
-    });
-    test('Unsuccessful Login with Locked out Credentials', async ({ loginPage, lockedUserData }) => {
         await loginPage.login(lockedUserData.username, lockedUserData.password);
         await loginPage.verifyErrorMessage('Epic sadface: Sorry, this user has been locked out.');
-    });
-    test('Bypass Login Attempt', async ({ loginPage }) => {
-        let targetUrl = '/inventory.html';
-        await loginPage.bypassLogin(targetUrl);
-        await loginPage.verifyErrorMessage(`Epic sadface: You can only access '${targetUrl}' when you are logged in.`);
+        await loginPage.bypassLogin('/inventory.html');
+        await loginPage.verifyErrorMessage(`Epic sadface: You can only access '/inventory.html' when you are logged in.`);
     });
 });
