ff815b4d939d8dd1fa92ab1e5e07ea4be46e21c5
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2025-11-12 20:13:55 +0800

    全部的業務邏輯都先放在一個案例，理論上要拆分為多個案例

---
+++ services/components/HamburgerMenu.ts
@@ -0,0 +1,51 @@
+import { type Page, type Locator } from '@playwright/test';
+
+export class HamburgerMenu {
+	readonly page: Page;
+	readonly menuButton: Locator;
+	readonly inventoryLink: Locator;
+	readonly aboutLink: Locator;
+	readonly logoutLink: Locator;
+	readonly resetAppStateLink: Locator;
+	readonly closeMenuButton: Locator;
+
+	constructor(page: Page) {
+		this.page = page;
+		this.menuButton = page.locator('#react-burger-menu-btn');
+		this.inventoryLink = page.locator('[data-test="inventory-sidebar-link"]');
+		this.aboutLink = page.locator('[data-test="about-sidebar-link"]');
+		this.logoutLink = page.locator('[data-test="logout-sidebar-link"]');
+		this.resetAppStateLink = page.locator('[data-test="reset-sidebar-link"]');
+		this.closeMenuButton = page.locator('#react-burger-cross-btn');
+	}
+
+	async openMenu() {
+		await this.menuButton.click();
+	}
+
+	async closeMenu() {
+		await this.closeMenuButton.click();
+	}
+
+	async goToProductPage() {
+		await this.openMenu();
+		await this.inventoryLink.click();
+	}
+
+	async goToAboutPage() {
+		await this.openMenu();
+		await this.aboutLink.click();
+	}
+
+	async logout() {
+		await this.openMenu();
+		await this.logoutLink.click();
+	}
+
+	async resetAppState() {
+		await this.openMenu();
+		await this.resetAppStateLink.click();
+		await this.closeMenu();
+		await this.page.reload();
+	}
+}
