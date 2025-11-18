32ccf3d7bd54cde664299b613bb612bdf1a8f4c7
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2025-11-18 21:06:10 +0800

    Product data parameterize

--- fixtures/TestData.fixture.ts
+++ fixtures/TestData.fixture.ts
@@ -7,6 +7,7 @@ type UserCredential = {
 };
 
 // 定義使用者憑證資料
+// as const 讓const物件變成readonly
 const credentials = {
 	standard: { username: 'standard_user', password: 'secret_sauce' },
 	lockedOut: { username: 'locked_out_user', password: 'secret_sauce' },
@@ -22,6 +23,7 @@ type ErrorDatas = {
 };
 
 // 定義錯誤訊息資料
+// as const 讓const物件變成readonly
 const loginErrorData = {
 	invalidCredentialsMessage:
 		'Epic sadface: Username and password do not match any user in this service',
@@ -30,12 +32,26 @@ const loginErrorData = {
 	bypassUrl: '/inventory.html',
 } as const;
 
+// 定義商品類型
+type Product = (typeof productsToAdd)[number];
+
+// 定義商品資料
+// as const 讓const陣列變成readonly
+const productsToAdd = [
+	'Sauce Labs Backpack',
+	'Sauce Labs Bolt T-Shirt',
+	'Sauce Labs Bike Light',
+] as const;
+const productToView = 'Sauce Labs Backpack' as const;
+
 // 定義資料fixture類型
 type DataFixtures = {
 	standardUserData: UserCredential;
 	lockedUserData: UserCredential;
 	invalidUserData: UserCredential;
 	loginErrorData: ErrorDatas;
+	productsToAdd: Product[];
+	productToView: Product;
 };
 
 // 擴展基本測試fixture以包含資料fixture
@@ -51,5 +67,11 @@ export const dataTest = baseTest.extend<DataFixtures>({
 	},
 	loginErrorData: async ({}, use) => {
 		await use({ ...loginErrorData });
+	},
+	productsToAdd: async ({}, use) => {
+		await use([...productsToAdd]);
+	},
+	productToView: async ({}, use) => {
+		await use(productToView);
 	},
 });
--- tests/Shopping.spec.ts
+++ tests/Shopping.spec.ts
@@ -1,135 +1,16 @@
 import { test } from '../fixtures/chainFixtures.js';
 
 test.describe('Shopping Scenarios', () => {
-	// test('登入進入檢視商品頁後返回商品主頁，加入指定商品到購物車，Reset App State 回到初始狀態，重新加入指定商品到購物車，進入購物車頁面確認商品，繼續購物，登出，再次登入，進入購物車頁面確認商品，導向商品主頁，進入購物車頁面確認商品，導向商品主頁，進入購物車頁面確認商品', async ({
-	// 	loginPage,
-	// 	standardUserData,
-	// 	productPage,
-	// 	cartPage,
-	// }) => {
-	// 	// 流程1: 登入
-	// 	await loginPage.login(standardUserData.username, standardUserData.password);
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點1: 確認已登入成功進入商品主頁
-	// 	await productPage.verifyOnProductListPage();
-	// 	// 驗證點2: 確認購物車圖示初始狀態無數量顯示
-	// 	await productPage.verifyCartIconNotVisible();
-
-	// 	const productName = 'Sauce Labs Backpack';
-
-	// 	// 流程2: 進入檢視商品頁後返回商品主頁
-	// 	await productPage.viewProductDetails(productName);
-	// 	await productPage.returnToProductList();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點: 確認已返回商品主頁
-	// 	await productPage.verifyOnProductListPage();
-
-	// 	const productsToAdd = [
-	// 		'Sauce Labs Backpack',
-	// 		'Sauce Labs Bike Light',
-	// 		'Sauce Labs Bolt T-Shirt',
-	// 	];
-
-	// 	// 流程3：加入指定商品到購物車
-	// 	await productPage.addMultipleProductsToCart(productsToAdd);
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點 1: 確認商品頁面按鈕狀態改變
-	// 	await productPage.verifyMultipleItemsStatusIsRemove(productsToAdd);
-	// 	// 驗證點 2: 確認購物車圖示數量正確
-	// 	await productPage.verifyCartItemCount(productsToAdd.length);
-
-	// 	// 流程4: Reset App State 回到初始狀態
-	// 	await productPage.hamburgerMenu.resetAppState();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點 1: 確認購物車圖示數量歸零
-	// 	await productPage.verifyCartIconNotVisible();
-
-	// 	// 流程5: 重新加入指定商品到購物車
-	// 	await productPage.addMultipleProductsToCart(productsToAdd);
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點 1: 確認商品頁面按鈕狀態改變
-	// 	await productPage.verifyMultipleItemsStatusIsRemove(productsToAdd);
-	// 	// 驗證點 2: 確認購物車圖示數量正確
-	// 	await productPage.verifyCartItemCount(productsToAdd.length);
-
-	// 	// 流程6：進入購物車頁面
-	// 	await productPage.goToCartPage();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點1: 確認購物車頁面商品正確
-	// 	await cartPage.verifyCartItems(productsToAdd);
-	// 	// 驗證點2: 確認購物車頁面商品數量正確
-	// 	await cartPage.verifyCartItemCount(productsToAdd.length);
-
-	// 	// 流程7: 繼續購物
-	// 	await cartPage.continueShopping();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點: 確認已返回商品主頁
-	// 	await productPage.verifyOnProductListPage();
-
-	// 	// 流程8: 登出
-	// 	await productPage.hamburgerMenu.logout();
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點: 確認已登出回到登入頁面
-	// 	await loginPage.verifyOnLoginPage();
-
-	// 	// 流程9: 再次登入
-	// 	await loginPage.login(standardUserData.username, standardUserData.password);
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點: 確認已登入成功進入商品主頁
-	// 	await productPage.verifyOnProductListPage();
-	// 	// 驗證點: 確認購物車圖示數量仍維持正確
-	// 	await productPage.verifyCartItemCount(productsToAdd.length);
-
-	// 	// 流程10: 進入購物車頁面確認商品
-	// 	await productPage.goToCartPage();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點1: 確認購物車頁面商品正確
-	// 	await cartPage.verifyCartItems(productsToAdd);
-	// 	// 驗證點2: 確認購物車頁面商品數量正確
-	// 	await cartPage.verifyCartItemCount(productsToAdd.length);
-
-	// 	// 流程11: 導向商品主頁
-	// 	await cartPage.hamburgerMenu.goToProductPage();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點: 確認已返回商品主頁
-	// 	await productPage.verifyOnProductListPage();
-	// 	// 驗證點: 確認購物車圖示數量仍維持正確
-	// 	await productPage.verifyCartItemCount(productsToAdd.length);
-
-	// 	// 流程12: 進入購物車頁面確認商品
-	// 	await productPage.goToCartPage();
-
-	// 	// --- 驗證點 (Assertion) ---
-	// 	// 驗證點1: 確認購物車頁面商品正確
-	// 	await cartPage.verifyCartItems(productsToAdd);
-	// 	// 驗證點2: 確認購物車頁面商品數量正確
-	// 	await cartPage.verifyCartItemCount(productsToAdd.length);
-	// });
-	// 拆分上面的測試案例
 	test('登入登出後購物車狀態持久性', async ({
 		loginPage,
 		standardUserData,
 		productPage,
 		cartPage,
+		productsToAdd,
 	}) => {
 		// 流程1: 登入
 		await loginPage.login(standardUserData.username, standardUserData.password);
 
-		const productsToAdd = [
-			'Sauce Labs Backpack',
-			'Sauce Labs Bike Light',
-			'Sauce Labs Bolt T-Shirt',
-		];
 		// 流程2：加入指定商品到購物車
 		await productPage.addMultipleProductsToCart(productsToAdd);
 
@@ -148,16 +29,16 @@ test.describe('Shopping Scenarios', () => {
 		// 驗證點2: 確認購物車頁面商品數量正確
 		await cartPage.verifyCartItemCount(productsToAdd.length);
 	});
-	test('重設應用程式狀態後購物車清空', async ({ loginPage, standardUserData, productPage }) => {
+	test('重設應用程式狀態後購物車清空', async ({
+		loginPage,
+		standardUserData,
+		productPage,
+		productsToAdd,
+	}) => {
 		// 流程1: 登入
 		await loginPage.login(standardUserData.username, standardUserData.password);
 
 		// 流程2：加入指定商品到購物車
-		const productsToAdd = [
-			'Sauce Labs Backpack',
-			'Sauce Labs Bike Light',
-			'Sauce Labs Bolt T-Shirt',
-		];
 		await productPage.addMultipleProductsToCart(productsToAdd);
 
 		// 流程3: 重設應用程式狀態
@@ -172,16 +53,16 @@ test.describe('Shopping Scenarios', () => {
 		standardUserData,
 		productPage,
 		cartPage,
+		productToView,
 	}) => {
 		// 流程1: 登入
 		await loginPage.login(standardUserData.username, standardUserData.password);
 
-		const productName = 'Sauce Labs Backpack';
-
-		await productPage.addProductToCart(productName);
+		// 加入指定商品到購物車
+		await productPage.addProductToCart(productToView);
 
 		// 流程2: 進入檢視商品頁後返回商品主頁
-		await productPage.viewProductDetails(productName);
+		await productPage.viewProductDetails(productToView);
 		await productPage.returnToProductList();
 		// 流程3：進入購物車頁面
 		await productPage.goToCartPage();
