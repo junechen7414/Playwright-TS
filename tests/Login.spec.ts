f042e82277bb5f86cc3c86cd80f6e1b522f54e7c
Bobby <90739416+junechen7414@users.noreply.github.com>
2025-12-13 21:51:37 +0800

    Revert "skip驗證登入失敗流程的test suite"
    This reverts commit dd7f3e53131d2b611f64b84221cc8d131a0076cf.

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,18 +1,17 @@
 import { test } from '../fixtures/chainFixtures.js';
 
-test.describe
-	.skip('Unsuccessful Login Scenarios', () => {
-		test('Unsuccessful Login with Invalid Credentials, Locked out Credentials and Bypass Login Attempt', async ({
-			loginPage,
-			invalidUserData,
-			lockedUserData,
-			loginErrorData,
-		}) => {
-			await loginPage.login(lockedUserData.username, lockedUserData.password);
-			await loginPage.verifyErrorMessage(loginErrorData.lockedOutCredentialsMessage);
-			await loginPage.login(invalidUserData.username, invalidUserData.password);
-			await loginPage.verifyErrorMessage(loginErrorData.invalidCredentialsMessage);
-			await loginPage.bypassLogin(loginErrorData.bypassUrl);
-			await loginPage.verifyErrorMessage(loginErrorData.bypassLoginMessage);
-		});
+test.describe('Unsuccessful Login Scenarios', () => {
+	test('Unsuccessful Login with Invalid Credentials, Locked out Credentials and Bypass Login Attempt', async ({
+		loginPage,
+		invalidUserData,
+		lockedUserData,
+		loginErrorData,
+	}) => {
+		await loginPage.login(lockedUserData.username, lockedUserData.password);
+		await loginPage.verifyErrorMessage(loginErrorData.lockedOutCredentialsMessage);
+		await loginPage.login(invalidUserData.username, invalidUserData.password);
+		await loginPage.verifyErrorMessage(loginErrorData.invalidCredentialsMessage);
+		await loginPage.bypassLogin(loginErrorData.bypassUrl);
+		await loginPage.verifyErrorMessage(loginErrorData.bypassLoginMessage);
 	});
+});
