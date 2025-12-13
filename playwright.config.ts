2428540fff329213fd0cd18baf5045b30a5ee392
Bobby <90739416+junechen7414@users.noreply.github.com>
2025-12-13 21:51:37 +0800

    Revert "在config中加入setup project測試檔案設定suffix要setup.ts的"
    This reverts commit ad10b159dcfac185278dc73521aec738caf99f52.

--- playwright.config.ts
+++ playwright.config.ts
@@ -58,10 +58,6 @@ export default defineConfig({
 	/* Configure projects for major browsers */
 	projects: [
 		{
-			name: 'setup',
-			testMatch: /.*\.setup\.ts/,
-		},
-		{
 			name: 'chromium',
 			use: {
 				...devices['Desktop Chrome'],
