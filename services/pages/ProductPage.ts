983f4353682ad616db8fef198289262977dc3403
Bobby <90739416+junechen7414@users.noreply.github.com>
2025-12-13 21:51:37 +0800

    Revert "產品頁的fixture加入前置步驟前往url"
    This reverts commit bd56ee458cb021ec80eeb6ade8e09b6c58e2ed27.

--- services/pages/ProductPage.ts
+++ services/pages/ProductPage.ts
@@ -63,9 +63,7 @@ export class ProductPage {
 	async verifyOnProductDetailPage() {
 		expect.soft(this.backToProductsButton).toBeVisible();
 	}
-	async gotoProductPage() {
-		await this.page.goto('/inventory.html');
-	}
+
 	async goToCartPage() {
 		await this.cartLink.click();
 	}
