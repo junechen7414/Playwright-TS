54ea1c0133981b00429404d853da15e6ba0d838b
BobbyOnIBM <ilovecrydad@gmail.com>
2026-01-09 14:37:00 +0800

    Inherit base api object

--- services/apis/ProductApiClient.ts
+++ services/apis/ProductApiClient.ts
@@ -1,4 +1,5 @@
 import type { APIRequestContext } from '@playwright/test';
+import { BaseApiClient } from './BaseApiClient';
 
 export interface CreateProductPayload {
 	title: string;
@@ -14,10 +15,11 @@ export interface UpdateProductPayload {
 	description?: string;
 }
 
-export class ProductApiClient {
+export class ProductApiClient extends BaseApiClient {
 	private readonly endpoint: string;
 
-	constructor(private readonly request: APIRequestContext) {
+	constructor(request: APIRequestContext) {
+		super(request);
 		this.endpoint = '/api/v1/products';
 	}
 
@@ -25,33 +27,31 @@ export class ProductApiClient {
 	 * Read產品資料
 	 */
 	async readProduct(id: number) {
-		const response = await this.request.get(`${this.endpoint}/${id}`);
-		return response;
+		return await this.get(`${this.endpoint}/${id}`);
 	}
 
 	/**
 	 * Create產品資料
 	 */
 	async createProduct(payload: CreateProductPayload) {
-		const response = await this.request.post(this.endpoint, {
+		return await this.post(this.endpoint, {
 			data: payload,
 		});
-		return response;
 	}
+
 	/**
 	 * Update產品資料
 	 */
 	async updateProduct(id: number, payload: UpdateProductPayload) {
-		const response = await this.request.put(`${this.endpoint}/${id}`, {
+		return await this.put(`${this.endpoint}/${id}`, {
 			data: payload,
 		});
-		return response;
 	}
+
 	/**
 	 * Delete產品資料
 	 */
 	async deleteProduct(id: number) {
-		const response = await this.request.delete(`${this.endpoint}/${id}`);
-		return response;
+		return await this.delete(`${this.endpoint}/${id}`);
 	}
 }
