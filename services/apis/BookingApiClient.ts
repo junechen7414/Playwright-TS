d70b097e676b5035609689aed1dc181d04f40d83
BobbyAtHome <ilovecrydad@gmail.com>
2026-01-11 13:17:59 +0800

    更新後的api object，同樣繼承base api object

---
+++ services/apis/BookingApiClient.ts
@@ -0,0 +1,94 @@
+import type { APIRequestContext } from '@playwright/test';
+import { BaseApiClient } from './BaseApiClient';
+
+export interface CreateBookingPayload {
+	firstname: string;
+	lastname: string;
+	totalprice: number;
+	depositpaid: boolean;
+	bookingdates: {
+		checkin: string;
+		checkout: string;
+	};
+	additionalneeds?: string;
+}
+
+export interface CreateBookingResponse {
+	id: number;
+	booking: CreateBookingPayload;
+}
+
+export interface UpdateBookingPayload {
+	firstname?: string;
+	lastname?: string;
+	totalprice?: number;
+	depositpaid?: boolean;
+	bookingdates?: {
+		checkin?: string;
+		checkout?: string;
+	};
+	additionalneeds?: string;
+}
+
+export interface UpdateBookingResponse {
+	id: number;
+	booking: CreateBookingPayload;
+}
+
+export class BookingApiClient extends BaseApiClient {
+	private readonly endpoint: string;
+
+	constructor(request: APIRequestContext) {
+		super(request);
+		this.endpoint = '/booking';
+	}
+
+	async getToken() {
+		const response = await this.post('/auth', {
+			data: {
+				username: 'admin',
+				password: 'password123',
+			},
+		});
+		const token = (await response.json()).token;
+		return `token=${token}`;
+	}
+
+	/**
+	 * Read產品資料
+	 */
+	async getBookingByFirstName(firstName: string) {
+		return await this.get(`${this.endpoint}?firstname=${firstName}`);
+	}
+	async getBookingById(id: number) {
+		return await this.get(`${this.endpoint}/${id}`);
+	}
+
+	/**
+	 * Create產品資料
+	 */
+	async createBooking(payload: CreateBookingPayload) {
+		return await this.post(this.endpoint, {
+			data: payload,
+		});
+	}
+
+	/**
+	 * Update產品資料
+	 */
+	async updateBooking(id: number, payload: UpdateBookingPayload) {
+		return await this.put(`${this.endpoint}/${id}`, {
+			data: payload,
+			headers: { Cookie: await this.getToken() },
+		});
+	}
+
+	/**
+	 * Delete產品資料
+	 */
+	async deleteBooking(id: number) {
+		return await this.delete(`${this.endpoint}/${id}`, {
+			headers: { Cookie: await this.getToken() },
+		});
+	}
+}
