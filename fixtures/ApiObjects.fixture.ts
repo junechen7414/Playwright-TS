00a58548762000d3be7e70e7e4fffb7dbb94d6cc
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2026-01-16 08:44:53 +0800

    fixture, spec file, api client object中各自再improve寫法做法

--- fixtures/ApiObjects.fixture.ts
+++ fixtures/ApiObjects.fixture.ts
@@ -43,6 +43,9 @@ export const bookingApiTest = baseTest.extend<BookingApiFixtures>({
 	},
 	bookingApiWithDataDeletedAfterward: async ({ bookingApi }, use) => {
 		const createResponse = await bookingApi.createBooking(newBookingPayload);
+		if (!createResponse.ok()) {
+			throw new Error('Failed to create booking');
+		}
 		const responseJson = await createResponse.json();
 		const bookingId = responseJson.bookingid;
 
@@ -50,6 +53,7 @@ export const bookingApiTest = baseTest.extend<BookingApiFixtures>({
 
 		await bookingApi.deleteBooking(bookingId).catch(() => {
 			// 忽略已經被測案刪除的情況
+			/*靜默處理*/
 		});
 	},
 	newBookingPayload: async ({ request: _ }, use) => {
