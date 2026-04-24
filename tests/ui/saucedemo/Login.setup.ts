184ce445b9cf9206352f40ad0baf8a18503dff79
BobbyLaptop <90739416+junechen7414@users.noreply.github.com>
2026-04-25 06:17:17 +0800

    setup storageState, playwright按需載入

--- tests/ui/saucedemo/Login.setup.ts
+++ tests/ui/saucedemo/Login.setup.ts
@@ -1,12 +1,20 @@
 import { test as setup } from '../../../services/fixtures/chain-fixtures.fixture';
 
-const authFileDir = '.auth/login.json';
+const authDir = '.auth';
 
-setup('登入後儲存狀態', async ({ page, loginPage, standardUserData, productPage }) => {
+setup(
+	'登入 Standard User 並儲存狀態',
+	async ({ page, loginPage, standardUserData, productPage }) => {
+		await page.goto('');
+		await loginPage.login(standardUserData.username, standardUserData.password);
+		await productPage.verifyOnProductListPage();
+		await page.context().storageState({ path: `${authDir}/standard_user.json` });
+	},
+);
+
+setup('登入 Problem User 並儲存狀態', async ({ page, loginPage, problemUserData }) => {
 	await page.goto('');
-	await loginPage.login(standardUserData.username, standardUserData.password);
-	await productPage.verifyOnProductListPage();
-
-	// 儲存狀態
-	await page.context().storageState({ path: authFileDir });
+	await loginPage.login(problemUserData.username, problemUserData.password);
+	// 這裡不檢查 productPage 因為會登入失敗，但可以儲存錯誤狀態或僅示範多帳號
+	await page.context().storageState({ path: `${authDir}/problem_user.json` });
 });
