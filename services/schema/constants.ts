99af4fb51ba7bc159b4a3f3586acdd7cb63790ac
BobbyAtHome <ilovecrydad@gmail.com>
2026-04-26 14:01:23 +0800

    enum封裝死資料，參照和對應後端實際enum

---
+++ services/schema/constants.ts
@@ -0,0 +1,15 @@
+export enum AccountStatus {
+  Active = 'Y',
+  Inactive = 'N',
+}
+
+export enum ProductSaleStatus {
+  Available = 1001,
+  OutOfStock = 1002,
+}
+
+export enum OrderStatus {
+  Pending = 1001,
+  Completed = 1002,
+  Cancelled = 1003,
+}
